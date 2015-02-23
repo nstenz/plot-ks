78ac9f891f994dc1e1f43abcf0bfe58950e89b58
Noah Stenz <noah.stenz.github@gmail.com>
2015-02-23 15:31:29 -0600

    all output now located in a single directory, can now specify this directory as input to reuse calculations

--- plot-ks.pl
+++ plot-ks.pl
@@ -1,8 +1,8 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
-use Cwd;
 use Getopt::Long;
+use Cwd qw(abs_path);
 use File::Path qw(remove_tree);
 
 # Hash used to perform reverse translations
@@ -40,11 +40,20 @@ my $ks_min = 0;
 my $ks_max = 3;
 my $exclude_zero;
 
+# Allow for reusing info from an old run
+my $input_is_dir = 0;
+
+# Name of output directory
+my $project_name = "plot-ks-".time();
+
 # Check that dependencies can be found in user's PATH
 my $r = check_path_for_exec("R");
 my $blat = check_path_for_exec("blat");
 my $transdecoder = check_path_for_exec("TransDecoder");
 my $kaks_calculator = check_path_for_exec("KaKs_Calculator");
+
+# Save how script was invoked
+my $settings = "@ARGV";
 
 # Parse command line options
 GetOptions(
@@ -60,160 +69,255 @@ GetOptions(
 
 # Error check input
 my $transcriptome = shift;
-#die "You must specify a transcriptome for input.\n" if (!defined($transcriptome));
-#die "Could not locate '$transcriptome'.\n" if (!-e $transcriptome);
 die "You must specify a transcriptome for input.\n".&usage if (!defined($transcriptome));
 die "Could not locate '$transcriptome'.\n".&usage if (!-e $transcriptome);
 
-# Determine working directory and root of filename
-(my $input_dir = $transcriptome) =~ s/(.*\/).*/$1/;
-(my $input_root = $transcriptome) =~ s/.*\/(.*)\..*/$1/;
+# Input is a previous run directory, reuse information
+$input_is_dir++ if (-d $transcriptome);
 
-if ($input_dir eq $input_root) {
-	$input_dir = getcwd()."/";
-	$input_root =~ s/(.*)\..*/$1/;
+my $input_root;
+if (!$input_is_dir) {
+	my $transcriptome_abs_path = abs_path($transcriptome);
+
+	# Initialize our working directory and create symlink to transcriptome
+	mkdir($project_name) if (!-e $project_name) || die "Could not create directory '$project_name': $!.\n";
+
+	$transcriptome =~ s/.*\///;
+	system("ln -s $transcriptome_abs_path $ENV{PWD}/$project_name/$transcriptome");
+	($input_root = $transcriptome) =~ s/(.*)\.\S+$/$1/;
+}
+else {
+	$project_name = $transcriptome;
+	my @contents = glob("$project_name/*");
+
+	# Determine which transcriptome name by looking for a symlink
+	my $found_name = 0;
+	foreach my $file (@contents) {
+		if (-l $file) {
+			$file =~ s/\Q$project_name\E\///;
+			$transcriptome = $file;
+			$found_name = 1;
+		}
+	}
+	($input_root = $transcriptome) =~ s/(.*)\.\S+$/$1/;
+
+	die "Could not locate transcriptome in '$project_name'.\n" if (!$found_name);
 }
 
-chdir($input_dir);
+chdir($project_name);
+
+# Change how we handle Ctrl+C
+$SIG{INT} = 'INT_handler';
+
+# Set up output file names
+my $blat_out = "self-blat.pslx";
+my $paralogs_seqs = "paralogs-t$match_length_threshold.atx";
+my $paralogs_ks = "paralogs-t$match_length_threshold-m$model.atx";
+my $paralogs_ka_ks_out = "paralogs-t$match_length_threshold-m$model.kaks";
+
+my $final_ks_values;
+if ($exclude_zero) {
+	$final_ks_values = "paralogs-t$match_length_threshold-m$model-no_zero.csv";
+}
+else {
+	$final_ks_values = "paralogs-t$match_length_threshold-m$model-with_zero.csv";
+}
+
+my $ks_plot_name;
+if ($exclude_zero) {
+	$ks_plot_name = "$input_root-t$match_length_threshold-m$model-no_zero-ks$ks_min-$ks_max.pdf";
+}
+else {
+	$ks_plot_name = "$input_root-t$match_length_threshold-m$model-with_zero-ks$ks_min-$ks_max.pdf";
+}
+
+# Print invocation
+logger("Invocation: perl plot-ks.pl $settings");
 
 # Run TransDecoder
-print "\nRunning TransDecoder on '$transcriptome'...\n";
-my $transdecoder_out_dir = "ks-plot-transdecoder";
-system("$transdecoder -t $transcriptome --workdir $transdecoder_out_dir") && die;
+my $transdecoder_out_dir = "transdecoder";
+if (!-e "$transcriptome.transdecoder.pep") {
+	#print "\nRunning TransDecoder on '$transcriptome'...\n";
+	logger("\nRunning TransDecoder on '$transcriptome'...");
+	system("$transdecoder -t $transcriptome --workdir $transdecoder_out_dir") && die;
 
-# Clean up unneeded files
-remove_tree($transdecoder_out_dir);
+	# Clean up unneeded files
+	remove_tree($transdecoder_out_dir);
 
-print "Completed TransDecoder.\n\n";
+	#print "Completed TransDecoder.\n\n";
+	logger("Completed TransDecoder.\n");
+}
+else {
+	#print "\nUsing TransDecoder output from previous run.\n";
+	logger("\nUsing TransDecoder output from previous run.");
+}
 
-# Run blat
-print "Running self blat...\n";
-system("$blat $transcriptome.transdecoder.pep $transcriptome.transdecoder.pep -prot -out=pslx $transcriptome.pslx -noHead") && die;
-print "Completed self blat.\n\n";
+# Check if we've already run blat
+if (!-e $blat_out) {
+	#print "Running self blat...\n";
+	logger("Running self blat...");
+	system("$blat $transcriptome.transdecoder.pep $transcriptome.transdecoder.pep -prot -out=pslx self-blat.pslx -noHead") && die;
+	#print "Completed self blat.\n\n";
+	logger("Completed self blat.\n");
+}
+else {
+	#print "Using blat output from previous run.\n\n";
+	logger("Using blat output from previous run in '$blat_out'.");
+}
 
 # Load transcriptome
 my %align = parse_fasta("$transcriptome.transdecoder.mRNA");
 
-print "Parsing self blat output...\n";
+# Check if we've already parsed blat output
+if (!-e $paralogs_seqs) {
 
-my $id = 0;
-my @output;
-my %queries;
-my %matches;
-open(my $blat_out, "<", "$transcriptome.pslx");
-while (my $line = <$blat_out>) {
-	chomp($line);
+	#print "Parsing self blat output...\n";
+	logger("Parsing self-blat output...");
 
-	# Split the line on tabs and extract the information we want
-	my @line = split("\t", $line);
-	my ($query_name, $match_name, $query_align, $match_align) = 
-		($line[9], $line[13], $line[21], $line[22]);
-	
-	# Check if requirements are met
-	if ($query_name ne $match_name) {
-		# Reverse translate amino acids to their corresponding nucleotides
+	my $id = 0;
+	my @output;
+	my %queries;
+	my %matches;
+	open(my $blat_out, "<", $blat_out);
+	while (my $line = <$blat_out>) {
+		chomp($line);
 
-		my @query_align =  split(",", $query_align);
-		my @match_align =  split(",", $match_align);
+		# Split the line on tabs and extract the information we want
+		my @line = split("\t", $line);
+		my ($query_name, $match_name, $query_align, $match_align) = 
+			($line[9], $line[13], $line[21], $line[22]);
+		
+		# Check if requirements are met
+		if ($query_name ne $match_name) {
+			# Reverse translate amino acids to their corresponding nucleotides
 
-		my $query_nuc_align = $align{$query_name};
-		my $match_nuc_align = $align{$match_name};
+			my @query_align =  split(",", $query_align);
+			my @match_align =  split(",", $match_align);
 
-		my $trans_query_align;
-		foreach my $align (@query_align) {
-			$trans_query_align .= reverse_translate({"DNA" => $query_nuc_align, "PROT" => $align});
-		}
+			my $query_nuc_align = $align{$query_name};
+			my $match_nuc_align = $align{$match_name};
 
-		my $trans_match_align;
-		foreach my $align (@match_align) {
-			$trans_match_align .= reverse_translate({"DNA" => $match_nuc_align, "PROT" => $align});
-		}
+			my $trans_query_align;
+			foreach my $align (@query_align) {
+				$trans_query_align .= reverse_translate({"DNA" => $query_nuc_align, "PROT" => $align});
+			}
 
-		$query_align = $trans_query_align;
-		$match_align = $trans_match_align;
+			my $trans_match_align;
+			foreach my $align (@match_align) {
+				$trans_match_align .= reverse_translate({"DNA" => $match_nuc_align, "PROT" => $align});
+			}
 
-		if (length($query_align) >= $match_length_threshold && length($match_align) >= $match_length_threshold) {
+			$query_align = $trans_query_align;
+			$match_align = $trans_match_align;
 
-			# Check that the match hasn't already been extracted
-			if (!exists($queries{"$match_name-$query_name"})) {
+			if (length($query_align) >= $match_length_threshold && length($match_align) >= $match_length_threshold) {
 
-				# Remove nucleotides to make length a multiple of 3
-				die "WHUT?\n" if (length($query_align) % 3 != 0);
+				# Check that the match hasn't already been extracted
+				if (!exists($queries{"$match_name-$query_name"})) {
 
-				my $name = "q_$query_name"."_t_$match_name";
-				my $pair = {'QUERY_ALIGN' => $query_align,
-							'MATCH_ALIGN' => $match_align,
-							'LENGTH' => length($query_align)};
+					# Remove nucleotides to make length a multiple of 3
+					die "WHUT?\n" if (length($query_align) % 3 != 0);
 
-				# Check if there is already a match between these two sequences
-				# if there is a match, the longer length one will be output
-				if (exists($matches{$name})) {
-					my $current_length = $matches{$name}->{'LENGTH'};
-					if ($current_length <= length($query_align)) {
+					my $name = "q_$query_name"."_t_$match_name";
+					my $pair = {'QUERY_ALIGN' => $query_align,
+								'MATCH_ALIGN' => $match_align,
+								'LENGTH' => length($query_align)};
+
+					# Check if there is already a match between these two sequences
+					# if there is a match, the longer length one will be output
+					if (exists($matches{$name})) {
+						my $current_length = $matches{$name}->{'LENGTH'};
+						if ($current_length <= length($query_align)) {
+							$matches{$name} = $pair;
+						}
+					}
+					else {
 						$matches{$name} = $pair;
 					}
+					$queries{"$query_name-$match_name"}++;
 				}
-				else {
-					$matches{$name} = $pair;
-				}
-				$queries{"$query_name-$match_name"}++;
 			}
 		}
 	}
-}
 
-# Prepare for output
-foreach my $key (sort { $a cmp $b} keys %matches) {
-	my $pair = $matches{$key};
-	push(@output, ">$key\n");
-	push(@output, "$pair->{QUERY_ALIGN}\n");
-	push(@output, "$pair->{MATCH_ALIGN}\n\n");
-	$id++;
-}
-
-die "No blat hits met the requirements.\n\n" if ($id == 0);
-print "Completed parsing blat output, $id blat hit(s) met the requirements.\n\n";
-
-open(my $output_file, ">", "$transcriptome.atx");
-print {$output_file} @output;
-close($output_file);
-
-# Run KaKs_Calculator to get Ks values
-print "Running KaKs_Calculator...\n";
-system("$kaks_calculator -i $transcriptome.atx -o $transcriptome.kaks -m $model >/dev/null") && die;
-print "Completed KaKs_Calculator.\n\n";
-
-# Open KaKs_Calculator output, parse Ks values and convert to .csv for R
-open(my $ka_ks_calculator_output, "<", "$transcriptome.kaks");
-open(my $ks_csv, ">", "$transcriptome.csv");
-while (my $line = <$ka_ks_calculator_output>) {
-	chomp($line);
-	my @line = split(/\s+/, $line);
-
-	# Skip first line containing headers
-	if ($. == 1) {
-		print {$ks_csv} "ks\n";
-		next;
+	# Prepare for output
+	foreach my $key (sort { $a cmp $b} keys %matches) {
+		my $pair = $matches{$key};
+		push(@output, ">$key\n");
+		push(@output, "$pair->{QUERY_ALIGN}\n");
+		push(@output, "$pair->{MATCH_ALIGN}\n\n");
+		$id++;
 	}
 
-	my $ks = $line[3];
-	$ks = 0 if ($ks eq "NA");
-	next if ($ks == 0 && $exclude_zero);
+	
+	#die "No blat hits met the requirements.\n\n" if ($id == 0);
+	#print "Completed parsing blat output, $id blat hit(s) met the requirements.\n\n";
+	if ($id == 0) {
+		logger("No blat hits met the requirements.\n");
+		exit(0);
+	}
+	logger("Completed parsing blat output, $id blat hit(s) met the requirements.\n");
 
-	print {$ks_csv} "$ks\n"
+	open(my $output_file, ">", $paralogs_seqs);
+	print {$output_file} @output;
+	close($output_file);
 }
-close($ks_csv);
-close($ka_ks_calculator_output);
+else {
+	#print "Using previously parsed blat output.\n\n";
+	logger("Using previously parsed blat output in '$paralogs_seqs'.");
+}
+
+# Check if we've run KaKs_Calculator before
+if (!-e $paralogs_ka_ks_out) {
+	# Run KaKs_Calculator to get Ks values
+	#print "Running KaKs_Calculator...\n";
+	logger("Running KaKs_Calculator...");
+	system("$kaks_calculator -i $paralogs_seqs -o $paralogs_ka_ks_out -m $model >/dev/null") && die;
+	#print "Completed KaKs_Calculator.\n\n";
+	logger("Completed KaKs_Calculator.\n");
+}
+else {
+	logger("Using KaKs_Calculator output from previous run in '$paralogs_ka_ks_out'.");
+}
+
+# Check if we've parsed Ks values before
+if (!-e $final_ks_values) {
+
+	# Open KaKs_Calculator output, parse Ks values and convert to .csv for R
+	open(my $ka_ks_calculator_output, "<", $paralogs_ka_ks_out);
+	open(my $ks_csv, ">", $final_ks_values);
+	while (my $line = <$ka_ks_calculator_output>) {
+		chomp($line);
+		my @line = split(/\s+/, $line);
+
+		# Skip first line containing headers
+		if ($. == 1) {
+			print {$ks_csv} "ks\n";
+			next;
+		}
+
+		my $ks = $line[3];
+		$ks = 0 if ($ks eq "NA");
+		next if ($ks == 0 && $exclude_zero);
+
+		print {$ks_csv} "$ks\n"
+	}
+	close($ks_csv);
+	close($ka_ks_calculator_output);
+}
 
 # Create PDF plot of output
-print "Creating Ks plot in R...\n";
-system("echo \"pdf(file='$input_root-ks.pdf'); 
-	data=read.csv('$transcriptome.csv'); 
+#print "Creating Ks plot in R...\n";
+logger("Creating Ks plot in R...");
+system("echo \"pdf(file='$ks_plot_name'); 
+	#data=read.csv('$transcriptome.csv'); 
+	data=read.csv('$final_ks_values'); 
 	dat1 <- data\\\$ks[data\\\$ks < $ks_max]; 
 	hist(dat1, breaks=seq($ks_min,$ks_max,by=$bin_size), 
 		main=expression(paste('K'[s], ' Plot for $transcriptome')), 
 		xlab=expression(paste('Pairwise', ' K'[s])), axes=T);\" | $r --no-save") && die;
-print "\nCompleted Ks plot.\n";
+#print "\nCompleted Ks plot.\n";
+logger("\nCompleted Ks plot.\n");
 
 sub reverse_translate {
 	my $settings = shift;
@@ -279,8 +383,43 @@ sub parse_fasta {
 	return %align;
 }
 
+sub logger {
+	my $msg = shift;
+
+	my $time = "[".localtime(time())."]";
+
+	# Allow for new lines and carriage returns before message
+	if ($msg =~ s/^\n//) {
+		$time = "\n$time";
+	}
+	elsif ($msg =~ s/^\r//) {
+		$time = "\r$time";
+	}
+
+	print "$time $msg\n"; 
+}
+
+sub INT_handler {
+	logger("\rKeyboard interupt detected, stopping analyses and cleaning up.");
+
+	# Move back into the directory script was called in
+	chdir("..");
+
+	# Try to delete directory five times, if it can't be deleted print an error message
+	my $count = 0;
+	until (!-e $project_name || $count == 5) {
+		$count++;
+
+		remove_tree($project_name, {error => \my $err});
+		sleep(1);
+	}
+	logger("Could not clean all files in './$project_name/'.") if ($count == 5);
+
+	exit(0);
+}
+
 sub usage {
-	return "Usage: perl $0 [TRANSCRIPTOME] [OPTIONS]...\n";
+	return "Usage: perl plot-ks.pl [TRANSCRIPTOME] [OPTIONS]...\n";
 }
 
 sub help {
@@ -297,13 +436,13 @@ Generate a Ks plot for a given transcriptome in fasta format
   -h, --help                        display this help and exit
 
 Examples:
-  perl $0 assembly.fa -b 0.01               generates a Ks (YN model) plot from [0, 3] using a bin size of 0.01, and contigs 
+  perl plot-ks.pl assembly.fa -b 0.01               generates a Ks (YN model) plot from [0, 3] using a bin size of 0.01, and contigs 
                                                     with at least 300 bp of homologous sequence
 
-  perl $0 assembly.fa -x -m NG              generates a Ks (NG model) plot from (0, 3] using a bin size of 0.05, and contigs 
+  perl plot-ks.pl assembly.fa -x -m NG              generates a Ks (NG model) plot from (0, 3] using a bin size of 0.05, and contigs 
                                                     with at least 300 bp of homologous sequence
 
-  perl $0 assembly.fa --ks_max 5 -t 500     generates a Ks (YN model) plot from [0, 5] using a bin size of 0.01, and contigs 
+  perl plot-ks.pl assembly.fa --ks_max 5 -t 500     generates a Ks (YN model) plot from [0, 5] using a bin size of 0.01, and contigs 
                                                     with at least 500 bp of homologous sequence
 
 Mail bug reports and suggestions to <noah.stenz.github\@gmail.com>
